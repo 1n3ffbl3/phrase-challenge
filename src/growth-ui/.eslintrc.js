ec8f19120cf9f063a791ebcbcece7814bb27ee73
1n3ffbl3 <mizgier.m@gmail.com>
2023-06-04 17:03:29 +0200

    Remove default setup. Fix eslint intellisense

--- src/growth-ui/.eslintrc.js
+++ src/growth-ui/.eslintrc.js
@@ -1,7 +1,10 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
-module.exports = {
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+const config = {
   root: true,
   'extends': [
     'plugin:vue/vue3-essential',
@@ -12,14 +15,19 @@ module.exports = {
   overrides: [
     {
       files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
+        'cypress/e2e/**/*.{cy,spec}.{ts,tsx}'
       ],
       'extends': [
         'plugin:cypress/recommended'
       ]
     }
   ],
+  ignorePatterns: [
+    ".gitignore"
+  ],
   parserOptions: {
     ecmaVersion: 'latest'
   }
 }
+
+module.exports = config;
