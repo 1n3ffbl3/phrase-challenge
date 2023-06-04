f73567db92f0c8f1b46e91030b3b03475bd4354c
1n3ffbl3 <mizgier.m@gmail.com>
2023-06-04 18:07:28 +0200

    Add empty view components, vuetify, i18n

--- src/growth-ui/src/main.ts
+++ src/growth-ui/src/main.ts
@@ -1,12 +1,33 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { createI18n } from 'vue-i18n'
+import { messages } from '../i18n/translations'
+
 import App from './App.vue'
 import router from './router'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const i18n = createI18n({
+  locale: 'en',
+  messages
+})
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
+app.use(i18n)
 
 app.mount('#app')
--- src/growth-ui/src/router/index.ts
+++ src/growth-ui/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ProjectsView from '../views/ProjectsView.vue'
+import CreateProjectView from '../views/CreateProjectView.vue'
+import EditProjectView from '../views/EditProjectView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +11,21 @@ const router = createRouter({
       name: 'home',
       component: ProjectsView
     },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: ProjectsView
+    },
+    {
+      path: '/projects/new',
+      name: 'new-project',
+      component: CreateProjectView
+    },
+    {
+      path: '/projects/:id/edit',
+      name: 'edit-project',
+      component: EditProjectView
+    }
   ]
 })
 
