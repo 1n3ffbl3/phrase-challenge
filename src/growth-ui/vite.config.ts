0e83f01c49ba81692be683919357554d668911a7
1n3ffbl3 <mizgier.m@gmail.com>
2023-06-05 18:25:46 +0200

    Add proxy for local development

--- src/growth-ui/vite.config.ts
+++ src/growth-ui/vite.config.ts
@@ -4,11 +4,36 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+/** @type {import('vite').UserConfig} */
+const config = defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8090",
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        // changeOrigin: true,
+        // secure: false,
+        // ws: true,
+        configure: (proxy) => {
+          proxy.on('error', (err) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (_, req) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      }
+    }
   }
 })
+
+export default config;
